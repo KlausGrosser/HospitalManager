1cca96d3d584b1295c771fd0be107af5dc710a9f
KlausGrosser <klaus.grosser@deliveryhero.com>
2022-02-07 12:31:55 +0100

    Implemented Deparment entity, service, repo

--- src/main/java/com/hospital/hospitalmanagement/department/DepartmentRepository.java
+++ src/main/java/com/hospital/hospitalmanagement/department/DepartmentRepository.java
@@ -1,7 +1,28 @@
 package com.hospital.hospitalmanagement.department;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
 
+    Optional<Department> findDepartmentByName(String departmentName);
+
+    Optional<Department> findDepartmentById(Long id);
+
+    @Transactional
+    @Modifying
+    @Query(
+            "UPDATE Department a "+
+                    "SET a.enabled = TRUE "+
+                    "WHERE a.name = ?1"
+    )
+    int enableDepartment(String departmentName);
+
+
+    void delete(Optional<Department> department);
 }
